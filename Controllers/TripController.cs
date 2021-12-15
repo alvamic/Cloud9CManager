ca7baffe61ca965303d93a0ff06fb29ccf53a160
Mike Alvarez <michaelalvarez1717@gmail.com>
2021-12-14 17:02:46 -0800

    create trip form 80%

---
+++ Controllers/TripController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C9_CostManager.Controllers
+{
+    public class TripController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
